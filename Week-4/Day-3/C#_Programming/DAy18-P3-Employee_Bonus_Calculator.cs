5602450c355f298298d5bb7ede8dbf2d747b39af
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-11 16:47:48 +0530

    Implement salary validation in bonus calculator
    Added validation for negative salary input.

--- Week-4/Day-3/C#_Programming/DAy18-P3-Employee_Bonus_Calculator.cs
+++ Week-4/Day-3/C#_Programming/DAy18-P3-Employee_Bonus_Calculator.cs
@@ -13,6 +13,12 @@ class Program
         Console.Write("Enter Experience (years): ");
         int experience = Convert.ToInt32(Console.ReadLine());
 
+         if (salary < 0)
+        {
+            Console.WriteLine("Error: Salary cannot be negative.");
+            return;
+        }
+
         double bonusPercentage;
 
         if (experience < 2)
@@ -30,4 +36,5 @@ class Program
         Console.WriteLine("Bonus: " + bonus);
         Console.WriteLine("Final Salary: " + finalSalary);
     }
+
 }
