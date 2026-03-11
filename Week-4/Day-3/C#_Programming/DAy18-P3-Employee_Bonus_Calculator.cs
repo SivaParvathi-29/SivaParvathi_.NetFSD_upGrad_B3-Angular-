1ce4e79bf66be86126d462fac712abedb83ce872
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-11 15:20:22 +0530

    Add files via upload

---
+++ Week-4/Day-3/C#_Programming/DAy18-P3-Employee_Bonus_Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Experience (years): ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+
+        double bonusPercentage;
+
+        if (experience < 2)
+            bonusPercentage = 0.05;
+        else if (experience <= 5)
+            bonusPercentage = 0.10;
+        else
+            bonusPercentage = 0.15;
+
+        double bonus = salary * bonusPercentage;
+
+        double finalSalary = salary + bonus;
+
+        Console.WriteLine("Employee: " + name);
+        Console.WriteLine("Bonus: " + bonus);
+        Console.WriteLine("Final Salary: " + finalSalary);
+    }
+}
