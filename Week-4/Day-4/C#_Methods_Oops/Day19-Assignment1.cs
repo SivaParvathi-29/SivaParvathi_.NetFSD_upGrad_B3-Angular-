3d80ca8bcb304e0922ec925cafcedf453606a43c
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-12 17:26:24 +0530

    Add files via upload

---
+++ Week-4/Day-4/C#_Methods_Oops/Day19-Assignment1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Product
+{
+    private int productId;
+    private string productName;
+    private double unitPrice;
+    private int qty;
+
+    public Product(int productId)
+    {
+        this.productId = productId;
+    }
+
+    public int ProductId
+    {
+        get { return productId; }
+    }
+
+    public string ProductName
+    {
+        get { return productName; }
+        set { productName = value; }
+    }
+
+    public double UnitPrice
+    {
+        get { return unitPrice; }
+        set { unitPrice = value; }
+    }
+
+    public int Quantity
+    {
+        get { return qty; }
+        set { qty = value; }
+    }
+
+    public void ShowDetails()
+    {
+        double total = unitPrice * qty;
+
+        Console.WriteLine("Product ID: " + ProductId);
+        Console.WriteLine("Product Name: " + ProductName);
+        Console.WriteLine("Unit Price: " + UnitPrice);
+        Console.WriteLine("Quantity: " + Quantity);
+        Console.WriteLine("Total Amount: " + total);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Product p = new Product(101);
+
+        p.ProductName = "Laptop";
+        p.UnitPrice = 50000;
+        p.Quantity = 2;
+
+        p.ShowDetails();
+    }
+}
---
+++ Week-4/Day-4/C#_Methods_Oops/Day19-P1-Calculator_Using_Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Calculator calc = new Calculator();
+
+        int addition = calc.Add(a, b);
+        int subtraction = calc.Subtract(a, b);
+
+        Console.WriteLine("Addition = " + addition);
+        Console.WriteLine("Subtraction = " + subtraction);
+    }
+}
