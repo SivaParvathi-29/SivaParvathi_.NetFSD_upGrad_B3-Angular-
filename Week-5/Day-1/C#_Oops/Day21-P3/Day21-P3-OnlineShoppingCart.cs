924cae84e0a5fc3c8d2ef31055fe5bfaa1e6fbc6
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-16 15:15:27 +0530

    Add files via upload

---
+++ Week-5/Day-1/C#_Oops/Day21-P3/Day21-P3-OnlineShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Product product;
+
+        // Electronics Product
+        product = new Electronics();
+        product.Name = "Laptop";
+        product.Price = 20000;
+
+        Console.WriteLine("Product: " + product.Name);
+        Console.WriteLine("Original Price: " + product.Price);
+        Console.WriteLine("Final Price after 5% discount = " + product.CalculateDiscount());
+
+        Console.WriteLine();
+
+        // Clothing Product
+        product = new Clothing();
+        product.Name = "T-Shirt";
+        product.Price = 1000;
+
+        Console.WriteLine("Product: " + product.Name);
+        Console.WriteLine("Original Price: " + product.Price);
+        Console.WriteLine("Final Price after 15% discount = " + product.CalculateDiscount());
+    }
+}
---
+++ Week-5/Day-1/C#_Oops/Day21-P3/Day21-P3-OnlineShoppingClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Product
+{
+    private double price;   // private field (data hiding)
+
+    public string Name { get; set; }
+
+    public double Price
+    {
+        get { return price; }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Price cannot be negative.");
+                return;
+            }
+            price = value;
+        }
+    }
+
+    // Virtual method
+    public virtual double CalculateDiscount()
+    {
+        return Price;
+    }
+}
+
+class Electronics : Product
+{
+    public override double CalculateDiscount()
+    {
+        return Price - (Price * 0.05);
+    }
+}
+
+class Clothing : Product
+{
+    public override double CalculateDiscount()
+    {
+        return Price - (Price * 0.15);
+    }
+}
+
