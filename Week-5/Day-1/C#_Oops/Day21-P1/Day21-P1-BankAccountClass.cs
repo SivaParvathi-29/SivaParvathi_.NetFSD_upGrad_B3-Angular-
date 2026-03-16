600fd30b81a74f59c11965b0f118c4277658b4b5
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-16 15:14:45 +0530

    Add files via upload

---
+++ Week-5/Day-1/C#_Oops/Day21-P1/Day21-P1-BankAccountClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class BankAccount
+{
+    // Private fields (Data Hiding)
+    private int accountNumber;
+    private double balance;
+
+    // Property for Account Number
+    public int AccountNumber
+    {
+        get { return accountNumber; }
+        set { accountNumber = value; }
+    }
+
+    // Property for Balance (Read Only outside)
+    public double Balance
+    {
+        get { return balance; }
+    }
+
+    // Deposit Method
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Invalid deposit amount.");
+            return;
+        }
+
+        balance = balance + amount;
+        Console.WriteLine("Amount Deposited: " + amount);
+        Console.WriteLine("Current Balance: " + balance);
+    }
+
+    // Withdraw Method
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Invalid withdrawal amount.");
+            return;
+        }
+
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient balance.");
+            return;
+        }
+
+        balance = balance - amount;
+        Console.WriteLine("Amount Withdrawn: " + amount);
+        Console.WriteLine("Current Balance: " + balance);
+    }
+}
---
+++ Week-5/Day-1/C#_Oops/Day21-P4/Day21-P4-VehicleRentalSyatem.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Vehicle vehicle;
+
+        // Car Rental
+        vehicle = new Car();
+        vehicle.Brand = "Toyota";
+        vehicle.RentalRatePerDay = 2000;
+
+        double carRental = vehicle.CalculateRental(3);
+
+        Console.WriteLine("Vehicle: Car");
+        Console.WriteLine("Total Rental = " + carRental);
+
+        Console.WriteLine();
+
+        // Bike Rental
+        vehicle = new Bike();
+        vehicle.Brand = "Yamaha";
+        vehicle.RentalRatePerDay = 800;
+
+        double bikeRental = vehicle.CalculateRental(3);
+
+        Console.WriteLine("Vehicle: Bike");
+        Console.WriteLine("Total Rental = " + bikeRental);
+    }
+}
