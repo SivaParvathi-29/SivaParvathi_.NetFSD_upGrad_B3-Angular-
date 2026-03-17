8af489637493bb487575e9edddbb311c716af600
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-17 17:03:29 +0530

    Add files via upload

---
+++ Week-5/Day-2/C#_Records_Structures/Set-2/Day22-P4-StackBasedUndoSystem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class StackUndo
+{
+    string[] stack;
+    int top = -1;
+    int size;
+
+    public StackUndo(int size)
+    {
+        this.size = size;
+        stack = new string[size];
+    }
+
+    public void Push(string action)
+    {
+        if (top == size - 1)
+        {
+            Console.WriteLine("Stack Overflow - Cannot add more actions.");
+            return;
+        }
+
+        top++;
+        stack[top] = action;
+        Console.WriteLine(action + " added.");
+        Display();
+    }
+
+    public void Pop()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack Underflow - Nothing to undo.");
+            return;
+        }
+
+        Console.WriteLine("Undo: " + stack[top]);
+        top--;
+        Display();
+    }
+
+    public void Display()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Current State: Empty");
+            return;
+        }
+
+        Console.WriteLine("Current State:");
+        for (int i = 0; i <= top; i++)
+        {
+            Console.WriteLine(stack[i]);
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        StackUndo editor = new StackUndo(10);
+
+        editor.Push("Type A");
+        editor.Push("Type B");
+        editor.Push("Type C");
+
+        editor.Pop(); 
+        editor.Pop(); 
+    }
+}
---
+++ Week-5/Day-2/C#_Records_Structures/Set-2/Day22-P5-EmployeeManagement.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+class Node
+{
+    public int empId;
+    public string name;
+    public Node next;
+
+    public Node(int id, string name)
+    {
+        this.empId = id;
+        this.name = name;
+        this.next = null;
+    }
+}
+
+class LinkedList
+{
+    Node head = null;
+
+    public void InsertBeginning(int id, string name)
+    {
+        Node newNode = new Node(id, name);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void InsertEnd(int id, string name)
+    {
+        Node newNode = new Node(id, name);
+
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public void Delete(int id)
+    {
+        if (head == null)
+        {
+            Console.WriteLine("List is empty.");
+            return;
+        }
+
+        if (head.empId == id)
+        {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null && temp.empId != id)
+        {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null)
+        {
+            Console.WriteLine("Employee not found.");
+            return;
+        }
+
+        prev.next = temp.next;
+    }
+
+    public void Display()
+    {
+        Node temp = head;
+
+        if (temp == null)
+        {
+            Console.WriteLine("Employee list is empty.");
+            return;
+        }
+
+        while (temp != null)
+        {
+            Console.WriteLine(temp.empId + " - " + temp.name);
+            temp = temp.next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList list = new LinkedList();
+
+        list.InsertEnd(101, "Siva");
+        list.InsertEnd(102, "Sirisha");
+        list.InsertEnd(103, "Mani");
+
+        list.Delete(102);
+
+        Console.WriteLine("Employee List After Deletion:");
+        list.Display();
+    }
+}
