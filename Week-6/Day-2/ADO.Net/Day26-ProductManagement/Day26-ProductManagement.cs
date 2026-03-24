e9e1f7a95f97831210bed743c80ae03c51086032
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-24 17:47:20 +0530

    Add files via upload

---
+++ Week-6/Day-2/ADO.Net/Day26-ProductManagement/Day26-ProductManagement.cs
@@ -0,0 +1,67 @@
+﻿using ConsoleApp2.Data;
+using ProductApp.Models;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        ProductDAL dal = new ProductDAL();
+
+        while (true)
+        {
+            Console.WriteLine("\n1.Insert\n2.View\n3.Update\n4.Delete\n5.Exit");
+            Console.Write("Enter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Product p = new Product();
+                    Console.Write("Name: ");
+                    p.ProductName = Console.ReadLine();
+                    Console.Write("Category: ");
+                    p.Category = Console.ReadLine();
+                    Console.Write("Price: ");
+                    p.Price = decimal.Parse(Console.ReadLine());
+
+                    dal.InsertProduct(p);
+                    Console.WriteLine("Inserted!");
+                    break;
+
+                case 2:
+                    var list = dal.GetAllProducts();
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine($"{item.ProductId} {item.ProductName} {item.Category} {item.Price}");
+                    }
+                    break;
+
+                case 3:
+                    Product u = new Product();
+                    Console.Write("ID: ");
+                    u.ProductId = int.Parse(Console.ReadLine());
+                    Console.Write("New Name: ");
+                    u.ProductName = Console.ReadLine();
+                    Console.Write("New Category: ");
+                    u.Category = Console.ReadLine();
+                    Console.Write("New Price: ");
+                    u.Price = decimal.Parse(Console.ReadLine());
+
+                    dal.UpdateProduct(u);
+                    Console.WriteLine("Updated!");
+                    break;
+
+                case 4:
+                    Console.Write("ID: ");
+                    int id = int.Parse(Console.ReadLine());
+                    dal.DeleteProduct(id);
+                    Console.WriteLine("Deleted!");
+                    break;
+
+                case 5:
+                    return;
+            }
+        }
+    }
+}
---
+++ Week-6/Day-2/ADO.Net/Day26-ProductManagement/ProductDAL.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Configuration;
+using ProductApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ConsoleApp2.Data
+{
+    public class ProductDAL
+    {
+        private readonly string connectionString;
+
+        public ProductDAL()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public void InsertProduct(Product product)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_InsertProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                cmd.Parameters.AddWithValue("@Category", product.Category);
+                cmd.Parameters.AddWithValue("@Price", product.Price);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> list = new List<Product>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetAllProducts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Product
+                    {
+                        ProductId = (int)reader["ProductId"],
+                        ProductName = reader["ProductName"].ToString(),
+                        Category = reader["Category"].ToString(),
+                        Price = (decimal)reader["Price"]
+                    });
+                }
+            }
+            return list;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductId", product.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                cmd.Parameters.AddWithValue("@Category", product.Category);
+                cmd.Parameters.AddWithValue("@Price", product.Price);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductId", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
