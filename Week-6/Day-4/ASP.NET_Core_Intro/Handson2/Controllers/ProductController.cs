da28a627720cfc516c99af19e4192f27e1498ff5
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-26 15:58:35 +0530

    Add files via upload

---
+++ Week-6/Day-4/ASP.NET_Core_Intro/Handson2/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+public class ProductController : Controller
+{
+    private List<Product> products = new List<Product>()
+    {
+        new Product { Id = 1, Name = "Laptop", Price = 50000 },
+        new Product { Id = 2, Name = "Mobile", Price = 20000 },
+        new Product { Id = 3, Name = "Tablet", Price = 30000 }
+    };
+
+    public IActionResult Index()
+    {
+        return View(products);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        return View(product);
+    }
+}
