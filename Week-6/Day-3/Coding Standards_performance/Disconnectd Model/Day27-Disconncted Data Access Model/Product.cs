9f8bb00f86af54176ecc0f98d9e70bafd34a6bda
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-26 12:20:43 +0530

    Delete Week-6/Day-3/Coding Standards_performance/Disconnectd Model/Day27-Disconncted Data Access Model directory

--- Week-6/Day-3/Coding Standards_performance/Disconnectd Model/Day27-Disconncted Data Access Model/Product.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Product
-{
-    public int ProductId { get; set; }
-    public string ProductName { get; set; }
-    public string Category { get; set; }
-    public decimal Price { get; set; }
-}
