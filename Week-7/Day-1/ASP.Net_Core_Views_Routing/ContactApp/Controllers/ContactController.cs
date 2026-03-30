3a0854ef3f9a2c24b02b3280bdc2495f13e9ad1d
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-30 17:33:30 +0530

    Add files via upload

---
+++ Week-7/Day-1/ASP.Net_Core_Views_Routing/ContactApp/Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace ContactApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        // In-memory list
+        static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        // 1. Show all contacts
+        public IActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        // 2. Get contact by ID
+        public IActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(x => x.ContactId == id);
+            return View(contact);
+        }
+
+        // 3. Add Contact (GET)
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        // 4. Add Contact (POST)
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
