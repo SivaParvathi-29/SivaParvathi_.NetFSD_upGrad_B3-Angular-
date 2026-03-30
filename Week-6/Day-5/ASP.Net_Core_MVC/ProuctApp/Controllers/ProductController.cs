d90d1c48310c4fae2f730734a28184a33e1c059c
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-30 13:33:03 +0530

    Add files via upload

---
+++ Week-6/Day-5/ASP.Net_Core_MVC/ProuctApp/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductApp1.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>();
+
+        // READ (Index)
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // CREATE - GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CREATE - POST
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        // DETAILS
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            return View(product);
+        }
+
+        // EDIT - GET
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            return View(product);
+        }
+
+        // EDIT - POST
+        [HttpPost]
+        public IActionResult Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = products.FirstOrDefault(x => x.ProductId == p.ProductId);
+                product.ProductName = p.ProductName;
+                product.Price = p.Price;
+                product.Category = p.Category;
+
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ProductId == id);
+            products.Remove(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Week-6/Day-5/ASP.Net_Core_MVC/ProuctApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5)]
+        public string Category { get; set; }
+    }
+}
