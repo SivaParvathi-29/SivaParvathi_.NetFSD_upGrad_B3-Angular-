7446cf94563bb029d4c8f03404413f13b589706d
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-03-31 17:04:15 +0530

    Add files via upload

---
+++ Week-7/Day-2/ASP.NET_Core_MVC_EFCore/P1-Student Registrtion/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Show Form
+        [HttpGet]
+        [Route("student/form")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        // POST: Handle Form
+        [HttpPost]
+        [Route("student/submit")]
+        public IActionResult Submit(string name, int age, string course)
+        {
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Course = course;
+
+            return View("Display");
+        }
+
+        // Display Data
+        [HttpGet]
+        [Route("student/display")]
+        public IActionResult Display()
+        {
+            return View();
+        }
+    }
+}
