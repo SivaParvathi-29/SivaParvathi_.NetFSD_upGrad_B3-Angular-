999a8926c24167880afaf03451fb09785f63eaa5
SivaParvathi-29 <sivaparvathipenugonda@gmail.com>
2026-04-29 10:47:06 +0530

    Add GradePipe for transforming grade numbers to strings

---
+++ Week-11/Day-1/Angular_Directives_Pipes/ContactManagementModule(Anguular)/Src/app/Pipes/grade-pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'grade',
+  standalone:true
+})
+export class GradePipe implements PipeTransform {
+
+  transform(value: number): string {
+    switch (value) {
+      case 1: return 'Outstanding';
+      case 2: return 'Excellent';
+      case 3: return 'Good';
+      case 4: return 'Average';
+      default: return 'Unknown';
+    }
+  }
+}
